3fd37561e5374cc78b22c5ecb71b4a4f715a3837
Mark Lewis <mlewis@trinity.edu>
2019-04-30 18:19:05 -0500

    Fixed up the react stuff we edited during class.

--- server/public/javascript/basicReact.js
+++ server/public/javascript/basicReact.js
@@ -14,17 +14,16 @@ let H1 = (props) => React.createElement('h1', props, 'Hello world!')
 class HelloWorld extends React.Component {
 	constructor(p) {
 		super(p);
-		state = {i: 5};
+		this.state = {i: 5};
 	}
 	foo() {
 		setState({i: 42});
 	}
 	render() {
-		return React.createElement('div', null, H1({style = 'color: "red"'}), H1);
+		return React.createElement('div', null, H1({style : {color: "red"}}), H1({style : {color: "green"}}));
 	}
 }
 ReactDOM.render(
 	React.createElement(HelloWorld, null),
 	document.getElementById('content')
 )
-*/
