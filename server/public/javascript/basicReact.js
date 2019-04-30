3e01ffdb538299141e96b7e49dc9ac2f94eadc0f
Mark Lewis <mlewis@trinity.edu>
2019-04-30 14:24:37 -0500

    In class changes to the basic react stuff.

--- server/public/javascript/basicReact.js
+++ server/public/javascript/basicReact.js
@@ -1,18 +1,26 @@
 /**
  * Some basic react stuff to demonstrate. 
  */
-
+/*
 ReactDOM.render(
   React.createElement('h1', null, 'Hello world!'),
   document.getElementById('content')
 )
 
-/* Here is a component based approach that creates two Hello Worlds.
+ Here is a component based approach that creates two Hello Worlds.
+*/
 
-let h1 = React.createElement('h1', null, 'Hello world!')
+let H1 = (props) => React.createElement('h1', props, 'Hello world!')
 class HelloWorld extends React.Component {
+	constructor(p) {
+		super(p);
+		state = {i: 5};
+	}
+	foo() {
+		setState({i: 42});
+	}
 	render() {
-		return React.createElement('div', null, h1, h1)
+		return React.createElement('div', null, H1({style = 'color: "red"'}), H1);
 	}
 }
 ReactDOM.render(
